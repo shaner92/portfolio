11a5ec57950db96182484fc2309ba7807bb26de7
shaner92 <46929382+shaner92@users.noreply.github.com>
2019-02-11 15:52:25 -0500

    Structure implremented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Header from './Header/Header.js';
+import About from './About/About.js'
+import Skills from './Skills/Skills.js'
+import Education from './Education/Education.js'
+import Experience from './Experience/Experience.js'
+import Projects from './Projects/Projects.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <About />
+        <Skills />
+        <Education />
+        <Experience />
+        <Projects />
       </div>
     );
   }
---
+++ src/Skills/Skills.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import '../bootstrap/css/bootstrap.min.css';
+import './Skills.scss'
+
+export default class Skills extends Component{
+    render(){
+        return(
+    <div>
+        
+        {/*Laptop image*/}
+        <div class="container-fluid p-0">
+            <div class="row">
+                <div class="col-md-12 text-center pt-5">
+                    <h1>Skills</h1>
+                    <p>
+                        I am an individual who is fascinated with learning. 
+                        I am passionately interested in Education, People, and Tech.
+                        My passion for education and learning makes me an active listener, 
+                        a curious individual, and inevitably a lifelong learner. 
+                        I see myself as a self-starter and a driven learner, 
+                        also enjoying a stimulating conversation, a strongly supported argument. 
+                        I enjoy the challenge of problem solving, analytical thinking , 
+                        being an innovative learner, and a versatile athlete. 
+                        My interests are also hiking, good music, 
+                        football and most importantly revamping my baking and cooking skills.</p>
+                </div> 
+            </div>
+        </div>
+
+        
+   
+    </div>)
+    }
+
+}
