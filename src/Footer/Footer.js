d5d0627c122566fdc814c212b954567830403bf8
shaner92 <46929382+shaner92@users.noreply.github.com>
2019-02-12 13:02:51 -0500

    Footer added

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../bootstrap/css/bootstrap.min.css';
+import './Footer.scss';
+
+export default class Footer extends Component{
+    render(){
+        return(
+    <div>
+      
+    </div>)
+    }
+
+}
